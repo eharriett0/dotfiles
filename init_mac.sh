4b378b574a0ca49f02de29bf06517b2fbb452c93
E Hont <quelo@Es-MacBook-Pro.lan>
2021-01-17 02:01:05 -0800

    init script

---
+++ init_mac.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+# homebrew 
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+exec $SHELL
+
+brew update 
+
+# iterm2
+brew install --cask iterm2
+
+# everything else (casks)
+brew install --cask notion
+brew install --cask zoomus
+brew install --cask brave-browser-dev
+brew install --cask visual-studio-code	
+brew install --cask spotify
+brew install --cask rectangle
+brew install --cask firefox-developer-edition
+brew install --cask wire
+brew install --cask discord
+brew install --cask nordvpn
+
+# go development
+export GOPATH="${HOME}/.go"
+export GOROOT="$(brew --prefix golang)/libexec"
+export PATH="$PATH:${GOPATH}/bin:${GOROOT}/bin"
+test -d "${GOPATH}" || mkdir "${GOPATH}"
+test -d "${GOPATH}/src/github.com" || mkdir -p "${GOPATH}/src/github.com"
+brew install go
+exec $SHELL
+go get golang.org/x/tools/cmd/godoc
+go get github.com/golang/lint/golint
+
+# rust development
+curl https://sh.rustup.rs -sSf | sh
+exec $SHELL
+source $HOME/.cargo/env
+
+# other tools
+brew install exa ripgrep
+brew tap homebrew/cask-fonts
+brew install --cask font-hack-nerd-font
