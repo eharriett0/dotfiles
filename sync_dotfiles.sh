39846c38d2b99603ba9eb81300c5bae88a66b357
Eddie <eharriet@villanova.edu>
2021-03-29 17:07:25 -0700

    ran sync_dotfiles

--- sync_dotfiles.sh
+++ sync_dotfiles.sh
@@ -3,3 +3,4 @@
 cp ~/.gitconfig /Users/quelo/repos/dotfiles/.gitconfig
 cp ~/.aliasrc /Users/quelo/repos/dotfiles/.aliasrc
 cp ~/.vimrc /Users/quelo/repos/dotfiles/.vimrc
+cp ~/.zshrc /Users/quelo/repos/dotfiles/.zshrc
