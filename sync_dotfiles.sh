065a767b2926dc500b8a46635bfb9d568bcab2ad
Eddie <eharriet@villanova.edu>
2021-03-29 17:02:55 -0700

    dotfiles

--- sync_dotfiles.sh
+++ sync_dotfiles.sh
@@ -1,1 +1,5 @@
-cp ~/.aliasrc .aliasrc
+#!/bin/bash
+
+cp ~/.gitconfig /Users/quelo/repos/dotfiles/.gitconfig
+cp ~/.aliasrc /Users/quelo/repos/dotfiles/.aliasrc
+cp ~/.vimrc /Users/quelo/repos/dotfiles/.vimrc
